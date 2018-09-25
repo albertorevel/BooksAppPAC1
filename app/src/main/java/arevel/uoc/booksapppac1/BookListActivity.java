c3370fc169157808c44ed68836a9121e1c2e6aae
Alberto Revel <albertorevel@users.noreply.github.com>
2018-09-26 00:36:11 +0200

    First commit

---
+++ app/src/main/java/arevel/uoc/booksapppac1/BookListActivity.java
@@ -0,0 +1,77 @@
+package arevel.uoc.booksapppac1;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class BookListActivity extends AppCompatActivity {
+
+    private final ArrayList<String> list = new ArrayList<String>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_list);
+        final ListView listview = findViewById(R.id.book_listview);
+
+        // Creamos la lista por defecto
+        for (int i = 0; i < 20; ++i) {
+            list.add("Item " + i);
+        }
+
+        StableArrayAdapter adapter = new StableArrayAdapter(getApplicationContext(),
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+//        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, final View view,
+//                                    int position, long id) {
+//                final String item = (String) parent.getItemAtPosition(position);
+//                view.animate().setDuration(2000).alpha(0)
+//                        .withEndAction(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                list.remove(item);
+//                                adapter.notifyDataSetChanged();
+//                                view.setAlpha(1);
+//                            }
+//                        });
+//            }
+//
+//        });
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+}
