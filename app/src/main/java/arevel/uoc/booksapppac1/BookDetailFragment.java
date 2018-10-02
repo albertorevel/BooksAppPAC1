faf6ce3b8b26b899bff9c62c632d5f70d216f054
Alberto Revel <albertorevel@users.noreply.github.com>
2018-10-03 00:22:51 +0200

    EX1 finished

--- app/src/main/java/arevel/uoc/booksapppac1/BookDetailFragment.java
+++ app/src/main/java/arevel/uoc/booksapppac1/BookDetailFragment.java
@@ -1,31 +1,42 @@
 package arevel.uoc.booksapppac1;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class BookDetailFragment extends Fragment {
 
     // Id que usaremos para saber el detalle del libro seleccionado
     private int bookId = -1;
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Obtenemos el id seleccionado para mostrar sus detalles
-        bookId = getArguments().getInt("BOOK_DETAIL_ID");
+        try {
+            bookId = getArguments().getInt("BOOK_DETAIL_ID");
+
+        } catch (NullPointerException npe) {
+            Log.e("DATA", "Error retrieving selected id");
+        }
+
+        // asociamos el layout del fragment al ViewGroup
+        View v = inflater.inflate(R.layout.book_detail_fragment, container, false);
 
         // Generamos el texto a mostrar con el id recuperado y lo asociamos al TextView que lo debe
         // mostrar
         String detailText = getResources().getString(R.string.list_detail_example) + " " + bookId;
 
-        TextView primaryTextView = getActivity().findViewById(R.id.primaryExampleTV);
+        TextView primaryTextView = v.findViewById(R.id.primaryExampleTV);
         primaryTextView.setText(detailText);
 
-        // asociamos el layout del fragment al ViewGroup
-        return inflater.inflate(R.layout.book_detail_fragment, container, false);
+        return v;
     }
 }
